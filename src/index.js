c46ab9c843fb4883a871a0ad69f79b940b453140
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-26 22:59:28 +0300

    qweqwe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // let KEY_KATE = live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ;
 
-import axios from "axios";
+// import axios from "axios";
 
-axios.defaults.headers.common["x-api-key"] = "live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ";
+// axios.defaults.headers.common["x-api-key"] = "live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ";

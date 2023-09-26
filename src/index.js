8283572b4c14087996eb1d9baed79ee632c87f9a
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-26 22:56:16 +0300

    start hw

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// let KEY_KATE = live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ;
+
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ";

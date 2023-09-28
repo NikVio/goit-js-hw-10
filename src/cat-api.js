81149cc53d136f0f93e7d15b15ea7cabed8e425e
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-28 20:40:22 +0300

    start

---
+++ src/cat-api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ";
+
+
+export function fetchBreeds() {
+    
+}
+
+export function fetchCatByBreed(breedId) {
+    
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-// let KEY_KATE = live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ;
 
-// import axios from "axios";
 
-// axios.defaults.headers.common["x-api-key"] = "live_3JjO3G5iaBcyeHeFwSPTqlukICTvmEXXsQ64luQc5BlpjgfQDzxuZTMvAaQIEsdZ";
+
+import SlimSelect from 'slim-select'
+
+new SlimSelect({
+  select: '#selectElement'
+})

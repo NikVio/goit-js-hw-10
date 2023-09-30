b440129b9dbd829db27c9d1ab8f8009fe60c0103
NikVio <131026216+NikVio@users.noreply.github.com>
2023-09-30 19:56:45 +0300

    continue hw10

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 
-
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 import SlimSelect from 'slim-select'
 
-new SlimSelect({
-  select: '#selectElement'
-})
+
+
+
+
+
+
+
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  container: document.querySelector('.cat-info')
+}
+
+console.log(refs);
+
+fetchBreeds().then(data => {
+  const creatMarcup = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
+  refs.select.insertAdjacentHTML('beforeend', creatMarcup);
+
+  new SlimSelect({
+    select: '#selectElement'
+  })
+  
+}).catch(err => {
+  refs.error.hidden = false;
+}).finally(() => refs.loader.hidden = false);
